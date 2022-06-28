4a5f3325ebe7f95020eedcdd6d49f18d4a5857fb
Marco <marcofienili004@gmail.com>
2022-06-28 13:02:57 +0200

    main

--- messerial.py
+++ messerial.py
@@ -1,17 +1,19 @@
 
 #! /usr/bin/python3
 
-#This program can Only read and print the serial buffer's data.
+#-------------------------------------------------------------------------------------------------------------
+#   This program can Only read and print the serial buffer's data.
 #
-#The software start from "" joins in "thereIsAboard()" and try to find a board.
-#If it find a board it start reading. 
-#If it doesen't find it join in "boardNotFound()" print an error.
-#In "toString" method you can customize the output string.
+#   The software start from "" joins in "thereIsAboard()" and try to find a board.
+#   If it find a board it start reading. 
+#   If it doesen't find it join in "boardNotFound()" print an error.
+#   In "toString" method you can customize the output string.
+#-------------------------------------------------------------------------------------------------------------
 
 import time 
 
-try:
-
+try:    
+    
     import serial
 
 except:
@@ -20,30 +22,37 @@ except:
     exit()
 
 #-------------------------------------------------------------------------------------------------------------
+#   DECLARATIONS
+#-------------------------------------------------------------------------------------------------------------
 
 maxTimes:int = 10 # max error times
 timeSleep:float = 1.0 # scheduling time
 
-#If not identify a board -------------------------------------------------------------------------------------------------------------
+port = "/dev/ttyACM0" # the port on which serial communication is to take place
+output = "OUTPUT: " #Customize the output
+
+#-------------------------------------------------------------------------------------------------------------
+
+#   If not identify a board
 
 def boardNotFound():
 
     if maxTimes == 0:
-        print("\nNO BOARD FOUND: exit.") #If it not indentify a board for 'timeSleep' times
+        print("\nNO BOARD FOUND on ",port,": exit.") #If it not indentify a board for 'timeSleep' times
         time.sleep(1.5)
         exit()
 
     else:
-        print("\nERROR: no board found")    #If it can't indentify a board
+        print("\nERROR: no board found on ",port)    #If it can't indentify a board
         print("Searching board...")
 
     time.sleep(1.5)
 
-#If indentify a board -------------------------------------------------------------------------------------------------------------
+#   If indentify a board
 
 def thereIsAboard():
 
-    board = serial.Serial("/dev/ttyACM0", 9600) #Detect if there is a board
+    board = serial.Serial(port, 9600) #Detect if there is a board
 
     print("\n\n---[BOARD FOUND]---")
     time.sleep(1.5)
@@ -55,18 +64,19 @@ def thereIsAboard():
 
         show(value)
 
-#OUTPUT -------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
+#   OUTPUT
+#-------------------------------------------------------------------------------------------------------------
 
-def toString(v):    #Customize the output
+def toString(v):    
+    return output, v #Customize the output
 
-    return "\nOUTPUT:\t" + v
-
-def show(v):
-    
+def show(v):    
     print(toString(v))
 
-#START -------------------------------------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------------------------------------
+#   START
+#-------------------------------------------------------------------------------------------------------------
 while True:
 
     try:
